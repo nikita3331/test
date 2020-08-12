e1d51d7c7609c19993b413460e9f1c71f65739c4
mykyta brazhynskyy <mykyta.brazhynskyy@gmail.com>
2020-08-12 18:49:54 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html')); 
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation.doc));
